de7ae0489972f4ad18286a16b4b7781e29bd6e80
Kyrre Havik <kyrremann@gmail.com>
2023-03-28 14:08:59 +0200

    feat: ny alert-modell
    Vi har faset ut Alerterator, så nå bygger den på PrometheusRules i stedet

--- main.go
+++ main.go
@@ -10,18 +10,12 @@ import (
 
 	"github.com/nais/historymanager/pkg/models"
 	"github.com/nais/historymanager/pkg/persister"
-	"github.com/nais/historymanager/pkg/v1alerts"
-	naisiov1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	"go.uber.org/zap"
-	"k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 type Root struct {
 	Logger *zap.Logger
-	Store  cache.Store
 }
 
 func (r *Root) history(w http.ResponseWriter, req *http.Request) {
@@ -44,7 +38,7 @@ func (r *Root) history(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	out, err := alertRequest.ToBigQuery(r.Store)
+	out, err := alertRequest.ToBigQuery()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		r.Logger.Error("Can't convert alert to BigQquery model", zap.Any("alert", json.RawMessage(bodyBytes)), zap.Error(err))
@@ -80,18 +74,6 @@ func main() {
 	r.Logger = logger
 	defer logger.Sync()
 
-	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-	if err != nil {
-		panic(err.Error())
-	}
-
-	naisiov1.AddToScheme(scheme.Scheme)
-	clientSet, err := v1alerts.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	r.Store = v1alerts.WatchAlerts(context.Background(), clientSet)
 	logger.Info("The ancient books slowly crumbled, their secrets turning to dust. But their every word sings within the BigQuery's head.")
 
 	http.HandleFunc("/history", r.history)
--- pkg/models/alert.go
+++ pkg/models/alert.go
@@ -1,83 +1,50 @@
 package models
 
 import (
-	"os"
-	"strings"
 	"time"
 
 	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/civil"
-	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
-	"k8s.io/client-go/tools/cache"
 )
 
 type Labels struct {
-	Alert               string
-	Alertname           string `json:"alertname"`
-	Deployment          string `json:"deployment"`
-	App                 string `json:"app"`
-	LogApp              string `json:"log_app"`
-	LogNamespace        string `json:"log_namespace"`
-	KubernetesName      string `json:"kubernetes_name"`
-	KubernetesNamespace string `json:"kubernetes_namespace"`
-	LinkerdDeployment   string `json:"linkerd_io_proxy_deployment"`
-	LinkerdNamespace    string `json:"linkerd_io_workload_ns"`
-	Namespace           string `json:"namespace"`
+	Alert          string
+	Alertname      string `json:"alertname"`
+	Namespace      string `json:"namespace"`
+	Deployment     string `json:"deployment"`
+	App            string `json:"app"`
+	LogApp         string `json:"log_app"`
+	KubernetesName string `json:"kubernetes_name"`
+	Cluster        string `json:"tenant_cluster"`
 }
 
 type Alert struct {
-	Status       string
-	Labels       Labels
-	Annotations  map[string]string
-	StartsAt     time.Time
-	EndsAt       time.Time
-	GeneratorUrl string
-	Fingerprint  string
+	Status      string
+	Labels      Labels
+	StartsAt    time.Time
+	EndsAt      time.Time
+	Fingerprint string
 }
 
 type AlertRequest struct {
-	Receiver          string
-	Status            string
-	Alerts            []Alert
-	GroupLabels       map[string]string
-	CommonLabels      map[string]string
-	CommonAnnotations map[string]string
-	ExternalUrl       string
-	Version           string
-	GroupKey          string
+	Receiver string
+	Status   string
+	Alerts   []Alert
 }
 
 type BigQueryAlert struct {
-	Alertname         string
-	Receiver          string
-	App               string
-	Namespace         string
-	TriggerdNamespace string                `bigquery:"triggered_namespace"`
-	StartsAt          civil.DateTime        `bigquery:"starts_at"`
-	EndsAt            bigquery.NullDateTime `bigquery:"ends_at"`
-	Status            string
-	Fingerprint       string
-	Cluster           string
+	Alertname   string
+	Receiver    string
+	App         string
+	Namespace   string
+	StartsAt    civil.DateTime        `bigquery:"starts_at"`
+	EndsAt      bigquery.NullDateTime `bigquery:"ends_at"`
+	Status      string
+	Fingerprint string
+	Cluster     string
 }
 
-func getFromStoreByDashKey(store cache.Store, name string) (string, error) {
-	for _, v := range store.ListKeys() {
-		key := strings.Replace(v, "/", "-", 1)
-		if key == name {
-			item, _, err := store.GetByKey(v)
-
-			if err != nil {
-				return "", err
-			}
-			naisAlert := item.(*nais_io_v1.Alert)
-			return naisAlert.Namespace, nil
-		}
-	}
-
-	return "", nil
-}
-
-func (a *Alert) AsBigQueryAlert(store cache.Store) (BigQueryAlert, error) {
+func (a *Alert) AsBigQueryAlert() (BigQueryAlert, error) {
 	startsAt, err := civil.ParseDateTime(a.StartsAt.Format("2006-01-02t15:04:05.999999999"))
 	if err != nil {
 		return BigQueryAlert{}, err
@@ -101,49 +68,29 @@ func (a *Alert) AsBigQueryAlert(store cache.Store) (BigQueryAlert, error) {
 		app = a.Labels.LogApp
 	}
 	if app == "" {
-		app = a.Labels.LinkerdDeployment
-	}
-	if app == "" {
 		app = a.Labels.Deployment
 	}
 	if app == "" {
 		app = a.Labels.KubernetesName
 	}
 
-	triggeredNamespace := a.Labels.Namespace
-	if triggeredNamespace == "" {
-		triggeredNamespace = a.Labels.LogNamespace
-	}
-	if triggeredNamespace == "" {
-		triggeredNamespace = a.Labels.LinkerdNamespace
-	}
-	if triggeredNamespace == "" {
-		triggeredNamespace = a.Labels.KubernetesNamespace
-	}
-
-	namespace, err := getFromStoreByDashKey(store, a.Labels.Alert)
-	if err != nil {
-		return BigQueryAlert{}, err
-	}
-
 	return BigQueryAlert{
-		Alertname:         a.Labels.Alertname,
-		Receiver:          a.Labels.Alert,
-		App:               app,
-		Namespace:         namespace,
-		TriggerdNamespace: triggeredNamespace,
-		StartsAt:          startsAt,
-		EndsAt:            nullEndsAt,
-		Status:            a.Status,
-		Fingerprint:       a.Fingerprint,
-		Cluster:           os.Getenv("NAIS_CLUSTER_NAME"),
+		Alertname:   a.Labels.Alertname,
+		Receiver:    a.Labels.Alert,
+		App:         app,
+		Namespace:   a.Labels.Namespace,
+		StartsAt:    startsAt,
+		EndsAt:      nullEndsAt,
+		Status:      a.Status,
+		Fingerprint: a.Fingerprint,
+		Cluster:     a.Labels.Cluster,
 	}, nil
 }
 
-func (ar *AlertRequest) ToBigQuery(store cache.Store) ([]BigQueryAlert, error) {
+func (ar *AlertRequest) ToBigQuery() ([]BigQueryAlert, error) {
 	var out []BigQueryAlert
 	for _, a := range ar.Alerts {
-		toBQ, err := a.AsBigQueryAlert(store)
+		toBQ, err := a.AsBigQueryAlert()
 		if err != nil {
 			return nil, err
 		}
